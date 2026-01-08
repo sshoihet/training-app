a0fca0a75b12660107fdacc07eabdda38217ba23
sshoihet <sshoihet@gmail.com>
2026-01-08 15:10:59 -0800

    Update courses.js

--- courses.js
+++ courses.js
@@ -79,7 +79,8 @@ const courseData = [
             { id: "v1", label: "Main Supply (V1)", size: "large", x: 17, y: 46, start: "open", correct: "closed" },
             
             // V2 & V3 are DRAINS -> Size: 'small'
-            { id: "v2", label: "Zone 1 Drain (V2)", size: "small", x: 41, y: 35, start: "closed", correct: "open" },
+            { id: "v2", label: "Zone 1 Drain (V2)", size: "small", x: 41, y: 10
+             , start: "closed", correct: "open" },
             { id: "v3", label: "Zone 2 Drain (V3)", size: "small", x: 50, y: 35, start: "closed", correct: "open" }
         ],
         quiz: {
@@ -91,5 +92,6 @@ const courseData = [
     }
 
 ];
+
 
 
